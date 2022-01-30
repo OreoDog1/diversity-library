81a0a9bdd3c846b47f6c1c8ad44008faddc950f2
OreoDog1 <zubintalwar@gmail.com>
2022-01-30 23:54:30 +0000

    Syntax

--- schema.sql
+++ schema.sql
@@ -1,34 +1,34 @@
 CREATE TABLE users (
-    id INTEGER
-    email TEXT
-    username TEXT
-    password_hash TEXT
+    id INTEGER,
+    email TEXT,
+    username TEXT,
+    password_hash TEXT,
     PRIMARY KEY(id)
 );
 
 
 CREATE TABLE books (
-    id INTEGER
-    title TEXT
-    author TEXT
-    description TEXT
+    id INTEGER,
+    title TEXT,
+    author TEXT,
+    description TEXT,
     -- store path to cover image
-    cover TEXT
+    cover TEXT,
     PRIMARY KEY(id)
 );
 
 
 CREATE TABLE tags (
-    book_id INTEGER
-    tag TEXT
+    book_id INTEGER,
+    tag TEXT,
     FOREIGN KEY(book_id) REFERENCES books(id)
 );
 
 
 CREATE TABLE requests (
-    user_id INTEGER
-    book_id INTEGER
-    status TEXT
-    FOREIGN KEY(user_id) REFERENCES users(id)
+    user_id INTEGER,
+    book_id INTEGER,
+    status TEXT,
+    FOREIGN KEY(user_id) REFERENCES users(id),
     FOREIGN KEY(book_id) REFERENCES books(id)
-)
+);
