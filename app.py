5d054faebce09c7cbbda4293e5de96c6507f3d60
OreoDog1 <zubintalwar@gmail.com>
2022-01-30 23:53:26 +0000

    Add register and set up

--- app.py
+++ app.py
@@ -1,7 +1,71 @@
+from cs50 import SQL
+from database import Database
 from flask import Flask, render_template
+from flask_session import Session
+from werkzeug.security import check_password_hash, generate_password_hash
 
 app = Flask(__name__)
+
+# Following three blocks from C$50 Finance
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+
+# Ensure responses aren't cached
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+
+# Configure session to use filesystem instead of signed cookies
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+db = Database(SQL("sqlite:///library.db"))
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route("/register", methods=["GET, POST"])
+def register():
+    # Provide form
+    if request.method == "GET":
+        return render_template("register.html")
+
+    # Make sure email was provided
+    email = request.form.get("email")
+    if not email:
+        flash("Email is Required")
+        return
+
+    # Make sure email isn't taken
+    if email in [user["email"] for user in db.get_users()]:
+        flash("Email taken")
+        return
+
+    # Make sure password was provided
+    password = request.form.get("password")
+    if not password:
+        flash("Password is required")
+        return
+
+    # Make sure passwords match
+    if password != request.form.get("confirmation"):
+        flash("Passwords don't match")
+        return
+
+    # Add info to database
+    db.create_user(email, generate_password_hash(password))
+
+    # Provide homepage
+    return redirect("/")
+
+
+
