fa68598c755942e6dba8fc1a2f52220fd50ea2dd
OreoDog1 <zubintalwar@gmail.com>
2022-02-02 04:58:51 +0000

    Added find_user

--- database.py
+++ database.py
@@ -8,4 +8,10 @@ class Database:
         return self.sql.execute("SELECT * FROM users")
 
     def create_user(self, email, password_hash)
-        self.sql.execute("INSERT INTO users (username, password_hash) VALUES(?, ?)", email, password_hash)
+        self.sql.execute("INSERT INTO users (email, password_hash) VALUES(?, ?)", email, password_hash)
+
+    def find_user(email):
+        try:
+            return self.sql.execute("SELECT * FROM users WHERE email = ?", email)[0]
+        except:
+            return None
